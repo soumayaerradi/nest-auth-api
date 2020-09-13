84a34edf83e08c020d1713a0b38c3266c59ae099
soumaya <s.erradi@codermine.com>
2020-09-13 18:33:51 +0200

    add error and logging handler

--- src/quote/quote.entity.ts
+++ src/quote/quote.entity.ts
@@ -2,9 +2,12 @@ import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeor
 
 @Entity('quote')
 export class QuoteEntity {
-    @PrimaryGeneratedColumn('uuid') id: string;
+    @PrimaryGeneratedColumn() id: string;
+
     @CreateDateColumn() created: Date;
+
     @Column('text') title: string;
+
     @Column('text') description: string;
 
 }
